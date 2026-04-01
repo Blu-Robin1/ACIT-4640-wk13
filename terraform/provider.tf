0fd80e423a41e0365cd6af58d38ef9e7bfb88063
satoisnice <connorbell015@gmail.com>
2026-04-01 15:07:13 -0700

    added lockfile and other

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
- backend "s3" {
-	bucket = "connor-omar-lab13"
-	key = "~/.ssh/aws"
-	region = "us-west-2"
-   }
+  backend "s3" {
+    bucket        = "connor-omar-lab13"
+    key           = "~/.ssh/aws"
+    region        = "us-west-2"
+    encrypt       = true
+    use_lockfile  = true
+  }
 }
 
 provider "aws" {
